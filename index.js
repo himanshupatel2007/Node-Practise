ae910620d5b0cb0ba0d21c3c63254e6c5521f065
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-04-05 22:43:34 +0530

    Fs module understand

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Node Practise Started")
+const  fs = require("fs") 
+console.log("server started")
+
+
+fs.writeFile("./information.txt","Hello from Nodejs",(error)=>{
+    if(error){
+        console.log("Error writing file")
+    }
+    else{
+        console.log("File written successfully")
+    }
+})
+
+fs.writeFileSync("./information.txt","Hello from Nodejs")
+fs.appendFile("./information.txt","Bye from NodeJs",(error)=>{
+    if(error)
+    {
+        console.log("Error writing file")
+    }
+    else{
+        console.log("file written successfully")
+    }
+})
+const result = fs.readFileSync("./contact.txt","utf-8")
+console.log(result)
