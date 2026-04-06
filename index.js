a59ceac89cfd3cc7e4ea4b3c7d497022df96235b
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-04-06 20:46:41 +0530

    several paths made using the URL

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const  fs = require("fs") 
-console.log("server started")
+const http = require("http");
+const fs = require("fs");
+const { error } = require("console");
 
+const myserver = http.createServer((req, res) => {
+  console.log("Request recieved in the server ");
+  const log = `request recieved at URL :${req.url}`;
+  fs.appendFile("./information.txt", log, (error) => {
+    if (error) {
+      console.log("Error writing file:", error);
+    }
+  });
+  switch (req.url) {
+    case "/home":
+      res.end("Response from Home Page");
+      break;
+    case "/about":
+      res.end("Response from About page");
+      break;
+    case "/contact":
+      res.end("response from Contact Page");
+      break;
 
-fs.writeFile("./information.txt","Hello from Nodejs",(error)=>{
-    if(error){
-        console.log("Error writing file")
-    }
-    else{
-        console.log("File written successfully")
-    }
-})
-
-fs.writeFileSync("./information.txt","Hello from Nodejs")
-fs.appendFile("./information.txt","Bye from NodeJs",(error)=>{
-    if(error)
-    {
-        console.log("Error writing file")
-    }
-    else{
-        console.log("file written successfully")
-    }
-})
-const result = fs.readFileSync("./contact.txt","utf-8")
-console.log(result)
+    default:
+      res.end("404 Not Found");
+  }
+});
+myserver.listen(8000, () => {
+  console.log("Server Started Successfully");
+});
