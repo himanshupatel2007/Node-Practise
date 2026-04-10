4efd9e5909977919f27668a83557c206242911f7
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-04-10 10:21:14 +0530

    user Login error fixed

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,32 +1,45 @@
 const User = require("../models/user");
 const fs = require("fs");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
-async function handleUserSignIn(req,res){
-    const {id,password} = req.body;
+async function handleUserSignIn(req, res) {
+ try {
+     const { first_name, password } = req.body;
 
-const user = await User.findById(id,(err)=>{
-    return res.status(400).json({
-        message :"User not found "
+  const user = await User.findOne({first_name});
+  if(!user){
+    console.log("User not found");
+    return res.status(404).json({
+        message:"User not found",
+        tip:"Please check user name correctly"
     })
-})
-const isMatch = await bcrypt.compare(user.password,password)
-if(isMatch)
-{fs.appendFile("../userSignIn.txt",`${req.body.id} logged in the Service`,(err)=>{
-    if(err){
-        console.log("error writing User Login")
-    }
-})
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    fs.appendFile(
+      "./userSignIn.txt",
+      `${req.body.first_name} logged in the Service`,
+      (err) => {
+        if (err) {
+          console.log("error writing User Login");
+        }
+      },
+    );
     return res.status(200).json({
-        message :"User Logged in successfully"
-    })
-
-}
-else{
+      message: "User Logged in successfully",
+    });
+  } else {
     return res.status(400).json({
-        message:"Password Incorrect"
+      message: "Password Incorrect",
+    });
+  }
+    
+ } catch (error) {
+    console.log("Error user SignIn");
+    res.status(500).json({
+        message:"internal server error signing User"
     })
-}
+ }
 }
 
-module.exports = handleUserSignIn
+module.exports = handleUserSignIn;
--- middlewares/passwordHash.js
+++ middlewares/passwordHash.js
@@ -1,9 +1,23 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
-async function EncryptPassword(req,res,next){
-    
-    const hashedpassword = bcrypt.hash(req.body.password);
-    req.body.password = hashedpassword
+async function EncryptPassword(req, res, next) {
+  try {
+    const password = req.body.password;
+    if (password) {
+      const hashedpassword = await bcrypt.hash(password,10);
+      req.body.password = hashedpassword;
+    } else {
+      return res.status(400).json({
+        message: "Password is required",
+      });
+    }
+
     next();
+  } catch (error) {
+    console.log("Error Hashing Password");
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 }
-module.exports = EncryptPassword
+module.exports = EncryptPassword;
