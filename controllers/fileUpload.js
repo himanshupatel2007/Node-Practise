9ed6ccede628af3c9249798e584b724f95ac728f
himanshupatel2007 <himanshupatel.942007@gmail.com>
2026-04-20 21:49:51 +0530

    remove a bug and improved request handling

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,10 +1,9 @@
 const fs = require("fs")
 
 function handleFileUpload(req, res) {
-    fs.appendFile("./fileUploads.txt", `${JSON.stringify(req.file)}- ${new Date().toISOString()}`,(err)=>{
+    fs.appendFile("./fileUploads.txt", `${JSON.stringify(req.file)}- ${new Date().toISOString()}\n`,(err)=>{
         if(err)
             console.log("Error logging Upload Log")
-        console.log("Error File upload Log");
     });
     res.status(201).json({
         message:"Succesfully Uploaded File"
